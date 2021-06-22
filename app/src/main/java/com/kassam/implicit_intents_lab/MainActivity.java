0f5a6425011e15c9a81d95950bbb19ca506e5dd2
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-06-22 16:54:48 +0300

    added calling functionality

--- app/src/main/java/com/kassam/implicit_intents_lab/MainActivity.java
+++ app/src/main/java/com/kassam/implicit_intents_lab/MainActivity.java
@@ -1,21 +1,23 @@
 package com.kassam.implicit_intents_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.ShareCompat;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mWebsiteEditText;
     private EditText mLocationEditText;
     private EditText mShareTextEditText;
+    private EditText mPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         mWebsiteEditText = findViewById(R.id.website_edittext);
         mLocationEditText = findViewById(R.id.location_edittext);
         mShareTextEditText = findViewById(R.id.share_edittext);
+        mPhoneNumber = findViewById(R.id.phone_number);
     }
 
     public void openWebsite(View view) {
@@ -68,5 +71,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void callNumber(View view) {
+        Uri u = Uri.parse("tel:"+ mPhoneNumber.getText().toString());
+        Intent i = new Intent(Intent.ACTION_DIAL, u);
+
+        try{
+            startActivity(i);
+        } catch (SecurityException s){
+            Toast.makeText(this, "An error occured", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
+        
+        
 }
